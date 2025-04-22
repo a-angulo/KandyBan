834fffda9173289236514dce79f7f9e4adeec138
a-angulo <alexabdoulah@gmail.com>
2025-04-21 22:38:33 -0700

    � Rename unused req to _req to prevent TS warning in wildcard route

--- server/src/server.ts
+++ server/src/server.ts
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(routes);
 
 // ✅ Catch-all: serve React for any unknown route
-app.get('*', (req, res) => {
+app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'client', 'dist', 'index.html'));
 });
 
